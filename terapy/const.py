40f99b0e38dc4288a1b1ad2a5d007058cc47cc5d
RockstarDevCuba <rockstarcu.dev@gmail.com>
2024-06-04 00:29:31 -0400

    Add const.py

---
+++ terapy/const.py
@@ -0,0 +1,29 @@
+BASE_URL = "https://www.terabox.app/share/list?app_id=250528&web=1&channel=0&jsToken={token}&dp-logid={log_id}&page=1&num=20&by=name&order=asc&site_referer=&shorturl={short_url}&root=1"
+
+TERABOX_URL = [
+        "1024terabox.com"
+        "www.mirrobox.com",
+        "www.nephobox.com",
+        "freeterabox.com",
+        "www.freeterabox.com",
+        "1024tera.com",
+        "4funbox.co",
+        "www.4funbox.com",
+        "mirrobox.com",
+        "nephobox.com",
+        "terabox.app",
+        "terabox.com",
+        "www.terabox.ap",
+        "www.terabox.com",
+        "www.1024tera.co",
+        "www.momerybox.com",
+        "teraboxapp.com",
+        "momerybox.com",
+        "tibibox.com",
+        "www.tibibox.com",
+        "www.teraboxapp.com",
+    ]
+
+
+TOKEN_PATTERN = r"fn%28%22(.*?)%22%29"
+DP_PATTERN = r"dp-logid=(.*?)&"
