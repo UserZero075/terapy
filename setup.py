89863654dbd6cc5c996e7b1a46b3eab82859062f
RockstarDevCuba <rockstarcu.dev@gmail.com>
2024-06-13 22:02:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup,find_packages
+from setuptools import setup
 
 
 fld = open("./README.md")
@@ -7,17 +7,17 @@ fld.close()
 
 setup(
     name="terapy",
-    version="0.1.2",
+    version="0.1.3",
     description="A library to download from terabox",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Diego Alberto Barreiro Delgado",
     author_email="rockstarcu.dev@gmail.com",
     url="https://github.com/RockstarDevCuba/terapy",
-    requires=[
-        "httpx",
-        "multidict",
-        "yarl"
+    install_requires=[
+        'yarl>=1.9.4',
+        'httpx>=0.27.0',
+        'multidict>=6.0.5',
     ],
-    package=find_packages()
+    packages="terapy"
 )
